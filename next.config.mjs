864cb47ea99942b40ab5756c219bea6c4131fd5e
Thalyson <thalison.wilker@gmail.com>
2025-11-18 02:52:28 -0300

    atualiza o workflow de deploy para usar Node.js 20, ajusta a instalação de dependências e melhora a configuração do Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  output: 'export',
+  images: { unoptimized: true },
+  // como o site está em https://pythonnortebrasil.github.io/site
+  // é recomendável:
+  basePath: '/site',
+  assetPrefix: '/site/',
+};
 
-export default nextConfig
+export default nextConfig;
